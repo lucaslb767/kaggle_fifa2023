b562773ba8c9a01d0e771cae637357939743cb18
LucasB <lucaslbarrozo@gmail.com>
2025-01-04 21:25:05 -0300

    creates filter for dataset

--- 1_home.py
+++ 1_home.py
@@ -1,5 +1,14 @@
 import streamlit as st
 import webbrowser
+import pandas as pd
+from datetime import datetime
+
+if "data" not in st.session_state:
+    df_data = pd.read_csv("datasets/CLEAN_FIFA23_official_data.csv", index_col= 0)
+    df_data = df_data[df_data['Contract Valid Until']>= 2023]
+    df_data = df_data[df_data['Value(£)']> 0]
+    df_data = df_data.sort_values(by="Overall", ascending=False)
+    st.session_state['data'] = df_data
 
 st.markdown("# Fifa 2023 Official Dataset")
 
